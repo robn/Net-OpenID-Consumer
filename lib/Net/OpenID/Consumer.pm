cbaf52dd953f83bed22d4e1a06407e3d4434dfcf
Robert Norris <rob@eatenbyagrue.org>
2010-10-26 09:28:41 +1100

    explicitly use crypt::dh::gmp

--- lib/Net/OpenID/Consumer.pm
+++ lib/Net/OpenID/Consumer.pm
@@ -34,7 +34,7 @@ use Net::OpenID::Common; # To get the OpenID::util package
 
 use MIME::Base64 ();
 use Digest::SHA1 ();
-use Crypt::DH 0.05;
+use Crypt::DH::GMP;
 use Time::Local;
 use HTTP::Request;
 
