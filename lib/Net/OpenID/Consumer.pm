42dcdca4ec426ee9b1fa071d023033f7dbf46a23
Robert Norris <rob@eatenbyagrue.org>
2010-10-26 09:17:03 +1100

    version bump and changelog update

--- lib/Net/OpenID/Consumer.pm
+++ lib/Net/OpenID/Consumer.pm
@@ -9,7 +9,7 @@ use Storable;
 package Net::OpenID::Consumer;
 
 use vars qw($VERSION);
-$VERSION = "1.03";
+$VERSION = "1.10";
 
 use fields (
     'cache',           # a Cache object to store HTTP responses and associations
